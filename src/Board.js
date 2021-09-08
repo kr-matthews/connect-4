1db3fdafc7416fa0dcb238793462c15f417af689
Kevin Matthews <kvnrmatthews@gmail.com>
2021-09-08 15:43:40 -0700

    More work on board, including initial styling.

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,63 @@
+import "./board.css";
+
 function Board({ board, placePiece, winningIndices, colours }) {
-  // TODO: Board component
+  // TODO: use placePiece
+  // TODO: fix winningIndices (probably refactor inside hook)
   let rows = board.length;
   var tableRows = [];
+  // the first row goes on the bottom, visually
   for (let row = rows - 1; row > -1; row--) {
-    tableRows.push(<Row key={row} row={board[row]} />);
+    tableRows.push(
+      <Row
+        key={row}
+        row={board[row]}
+        colours={colours}
+        winningIndices={winningIndices}
+      />
+    );
   }
 
   return (
-    <table>
+    <table className="board">
       <tbody>{tableRows}</tbody>
     </table>
   );
 }
 
-function Row({ row }) {
-  var rowCells = row.map((cell, col) => {
-    return <Cell key={col} cell={cell} />;
+function Row({ key, row, colours, winningIndices }) {
+  var rowCells = row.map((player, col) => {
+    return (
+      <Cell
+        key={col}
+        player={player}
+        colours={colours}
+        winningCell={winningIndices[key][col]}
+      />
+    );
   });
+
   return <tr>{rowCells}</tr>;
 }
 
-function Cell({ cell }) {
-  return <td>{cell}</td>;
+function Cell({ player, colours, winningCell }) {
+  function styleColour() {
+    let backgroundColor = colours[player];
+    let borderColor = winningCell
+      ? oppositeColour(backgroundColor)
+      : backgroundColor;
+    return { backgroundColor, borderColor };
+  }
+
+  return (
+    <td className="cell">
+      {player !== null && <span className="piece" style={styleColour()}></span>}
+    </td>
+  );
+}
+
+function oppositeColour(colour) {
+  // TODO: calculate opposite colour (just using DarkGray for now)
+  return "DarkGray";
 }
 
 export default Board;
